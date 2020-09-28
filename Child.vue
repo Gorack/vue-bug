a53949e4f8f7ac3fda5fb32090fe6f641197263c
Pintér Szabolcs <szabolcs.pinter@w5labs.com>
2020-09-28 08:34:11 +0200

    add example

---
+++ Child.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>Child component...</p>
+	</div>
+</template>
+
+<script lang="ts">
+    import {Component, Ref, Vue} from 'vue-property-decorator';
+
+    @Component({
+        name: 'Child'
+    })
+    export default class Child extends Vue {
+        childMethod() {
+            console.log('childMethod called');
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Parent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<p>Parent</p>
+		<Child ref="child"></Child>
+	</div>
+</template>
+
+<script lang="ts">
+    import {Component, Ref, Vue} from 'vue-property-decorator';
+    import Child from './Child.vue';
+
+    @Component({
+        name: 'Parent',
+	    components: {Child}
+    })
+    export default class Parent extends Vue {
+        @Ref('child') readonly child: Child;
+
+        mounted() {
+            this.child.childMethod();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
